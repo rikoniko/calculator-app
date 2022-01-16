fbd6f993b90e606d1e8bdffcab5cdb878955906b
rikoniko <rikoniko@com.com>
2022-01-16 15:24:03 +0900

    updata 四則演算の規則（乗除→加減）に従うように機能修正

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -1,53 +1,71 @@
-import 'package:flutter/material.dart';
+
 const c_op = ['+', '-', '×', '÷'];
 
 class Calculator{
-  static var _number = <double>[];
-  static var _op = <String>[];
-  static var _buffer;
+  static var number = <double>[];
+  static var op = <String>[];
+  static var buffer;
 
   static void GetKey(String letter){
-    // 四則演算子
+    // 数字と符号を分割
     if(c_op.contains(letter)){
-      _op.add(letter);
+      op.add(letter);
       //String型からdouble型に変換
-      _number.add(double.parse(_buffer));
-      _buffer = '';
+      number.add(double.parse(buffer));
+      print('number:$number');
+      buffer = '';
     } // C
     else if(letter == 'C'){
-      _number.clear();
-      _op.clear();
-      _buffer = '';
+      number.clear();
+      op.clear();
+      buffer = '';
     } // =
     else if(letter == '='){
       return null;
     }// 数字
     else{
-      _buffer += letter;
+      buffer += letter;
     }
   }
-  static var _result;
+
+  //四則演算
   static String Execute() {
-    _number.add(double.parse(_buffer));
-    if (_number.length == 0)
+    double tmp;
+    double result=0.0;
+    number.add(double.parse(buffer));
+    if (number.length == 0)
       return '0';
-    _result = _number[0];
-    for (int i = 0; i < _op.length; i++) {
-      if (_op[i] == '+')
-        _result +=_number[i + 1];
-      else if (_op[i] == '-')
-        _result -= _number[i + 1];
-      else if (_op[i] == '×')
-        _result *= _number[i + 1];
-      else if (_op[i] == '÷' && _number[i + 1] != 0)
-        _result /= _number[i + 1];
-      else
-        return 'e';
+
+    for(var i=0;i<op.length;i++) {
+      switch (op[i]) {
+        case '×':
+           tmp=number[i]*number[i + 1];
+           number[i]=0.0;
+           number[i+1]=tmp;
+          break;
+        case '÷':
+          tmp=number[i]/number[i + 1];
+          number[i]=0.0;
+          number[i+1]=tmp;
+          break;
+        case '-':
+          number[i + 1] =number[i + 1]*-1.0;
+          break;
+      }
     }
-    _number.clear();
-    _op.clear();
-    _buffer = '';
-    var resultStr = _result.toString().split('.');
-    return resultStr[1] == '0' ? resultStr[0] : _result.toString();
+
+    //最後に全て足す
+    for(double num in number){
+      result+=num;
+    }
+    //print('結果：$result');
+
+    number.clear();
+    op.clear();
+    buffer = '';
+
+    var resultStr = result.toString().split('.');
+    return resultStr[1] == '0' ? resultStr[0] : result.toString();
+
   }
 }
