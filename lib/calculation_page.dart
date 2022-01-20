b5336ae5bbf1db84444ca0d204edbd081c206352
rikoniko <rikoniko_777@icloud.com>
2022-01-20 09:26:50 +0900

    add ホーム画面のファイルとホーム画面から電卓画面の遷移を実装

---
+++ lib/calculation_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('電卓'),
+      ),
+      body:Container(
+        color: Colors.amberAccent,
+      )
+    );
+  }
+}
