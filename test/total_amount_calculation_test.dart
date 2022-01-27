325a9d2224cdd5bfb5e0a7170275f5943a166ada
rikoniko <rikoniko_777@icloud.com>
2022-01-28 00:05:05 +0900

    add 割引計算のテスト

--- test/total_amount_calculation_test.dart
+++ test/total_amount_calculation_test.dart
@@ -4,6 +4,22 @@ void main() {
   test('total amount calculation unit test', () {
     String productNumber="1";
     String productPrice="200";
-    expect(,);
+    String discountNumber="50";
+    String discountMethod="%OFF";
+    double? floorItem;
+    int sum=0;
+
+    switch (discountMethod){
+      case "割引":
+        floorItem=int.parse(productPrice)*(1-int.parse(discountNumber)*0.1)*int.parse(productNumber);
+        sum+=floorItem.floor();
+        break;
+      case "%OFF":
+        floorItem=int.parse(productPrice)*(1-int.parse(discountNumber)*0.01)*int.parse(productNumber);
+        sum+=floorItem.floor();
+        break;
+    }
+
+    expect(sum,100);
   });
 }
